0b8826af0674aac3ec46c963c3c80495f5a94bd3
Andrew C <candetandrei@gmail.com>
2019-06-02 19:00:05 +0300

    Create README.m

---
+++ LightHouse - Bill generator/README.m
@@ -0,0 +1,23 @@
+Hey!
+
+Due to old implementations and loss of web server, the software is not able to run properly. At local level (when there is "no internet connection" forced) the program would not start due to a minor problem to local DataBase file.
+
+Source code coming soon, I have to figure it out and sort the files and maybe the errors (and cut the connection to my old server).
+
+---> What was LightHouse all about?
+
+Well, long time ago, somebody asked me if I can build something easy to use to manage his subscribers to water-distributing company. LightHouse was automatically programmed to generate bills, sanction subscribers with old, unpaid bills and AUTO PRINT their bills. All he had to do was to pick up the printed bills and send them, also registering their water consumption index.
+
+The connection to the server was required to assure there is a license that I can manage and stop anti-fraud and software re-sharing. The server was albe to send a signal which could make the software pure useless in the case I decided to stop the collaboration OR stop the fraud.
+
+Back then in 2016, GDPR wasn't a thing, and subscribers data could be kinda compromised. This software uses a local DB for information, but I had the idea to encrypt the file and put a password on it (smart a$$ me back then). Pretty much the password was hardcoded and it requires good skills to find it, but I doubtly assume that there is somebody who will struggle to decrypt it to get how much water somebody used.
+
+---> What are my plans with LightHouse>
+
+Well, some years passed and I'm still thinking what should I do with it. The software was never really used by that man, because he tried to scam me and get the software for free.
+I am planning to rebuild this entire software and make it for different domains and customizable for everybody. It was pretty fixed to this domain (water distributing). Till then, I have to find a good team to make this possible.
+There is a need of this kind of softwares nowadays, where small companies cannot manage their clients and billings properly and hiring people to do this job may be exhausting for them and time consuming, resulting in a slow process and maybe mistakes.
+
+Thanks for throwing an eye over here and inspecting my work. I regret that I didn't take care of the source code and all the assets, but the scam kinda took me to a bad mood and decided to move to another domain where I could not be scammed.
+
+Have a nice day full of pizzah! 🍕
