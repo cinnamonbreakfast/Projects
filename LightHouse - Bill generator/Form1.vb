d99c8eaa5062b24f19d6673aac3958cc107171f1
Endru <candetandrei@gmail.com>
2019-06-02 18:42:12 +0300

    LightHouse added

---
+++ LightHouse - Bill generator/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class MainForm
+    Private IsFormBeingDragged As Boolean = False
+    Private MouseDownX As Integer
+    Private MouseDownY As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = System.Drawing.ColorTranslator.FromHtml("#2d2d30")
+    End Sub
+
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseDown
+
+        If e.Button = MouseButtons.Left Then
+            IsFormBeingDragged = True
+            MouseDownX = e.X
+            MouseDownY = e.Y
+        End If
+    End Sub
+
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseUp
+
+        If e.Button = MouseButtons.Left Then
+            IsFormBeingDragged = False
+        End If
+    End Sub
+
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseMove
+
+        If IsFormBeingDragged Then
+            Dim temp As Point = New Point()
+
+            temp.X = Me.Location.X + (e.X - MouseDownX)
+            temp.Y = Me.Location.Y + (e.Y - MouseDownY)
+            Me.Location = temp
+            temp = Nothing
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Load(sender As Object, e As EventArgs) Handles PictureBox1.Load
+        Me.BackColor = Color.Transparent
+
+    End Sub
+End Class
